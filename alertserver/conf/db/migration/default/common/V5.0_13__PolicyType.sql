1c8e62867ecf3cb0e64910a483c3ab2280470123
Pankaj Bahuguna <Pankaj.Bahuguna@alertenterprise.com>
2020-03-30 22:10:21 +0530

    updated data

--- alertserver/conf/db/migration/default/common/V5.0_13__PolicyType.sql
+++ alertserver/conf/db/migration/default/common/V5.0_13__PolicyType.sql
@@ -1,2 +1,2 @@
 INSERT INTO public.policy_type (id,int_status,changed_by,changed_on,created_by,created_on,description,ext_id,"text",doc_number) VALUES 
-(1,0,NULL,NULL,NULL,NULL,'Enterprise Access Role Policy','POL1','VALIDATION',NULL);
+(1,0,NULL,NULL,NULL,NULL,'Validation Policies','VALPOL','VALIDATION',NULL);
--- alertserver/conf/db/migration/default/common/V5.0_14__PolicyGroup.sql
+++ alertserver/conf/db/migration/default/common/V5.0_14__PolicyGroup.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.policy_group (id,changed_by,changed_on,created_by,created_on,int_status,description,doc_number,ext_id,"text") VALUES 
-(1,NULL,NULL,NULL,NULL,0,'Policy Group for Requests',NULL,'PGRP1','Request')
+(1,NULL,NULL,NULL,NULL,0,'Policy Group for Access Requests',NULL,'PGRPRequests','Access Requests')
 ;
