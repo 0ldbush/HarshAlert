8a875176b03a3fdcf7087da1ed6822a7c60a6247
Pankaj Bahuguna <Pankaj.Bahuguna@alertenterprise.com>
2020-04-03 12:43:01 +0530

    updated script for field def

--- alertserver/conf/evolutions/sqlserver/2.sql
+++ alertserver/conf/evolutions/sqlserver/2.sql
@@ -39,22 +39,22 @@ INSERT INTO aehsc.class_def (id,int_status,bus_obj_cat,ext_id,field_in_parent,pa
 ,(16,0,'DocNumberRange','docNumberRange',NULL,NULL,NULL,'Document Number Range','false',NULL,NULL,NULL,NULL,NULL,NULL,NULL)
 ,(11,0,'FieldDef','fieldDef',NULL,NULL,NULL,'Field Definition','false',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 
-INSERT INTO aehsc.field_def (id,class_ext_id,do_not_copy,field_name,is_base_class_field,is_dimension,is_list,is_measure,is_primitive,is_required,is_translated,is_unique,"label",no_history,reference_key,tooltip,"type",xtype) VALUES 
-(1,'request','false','roleData','false','false','true','false','false','false','false','false','Role Data','false',NULL,NULL,'role',NULL)
-,(2,'role','false','roleName','false','false','false','false','true','false','false','false','roleName/Role Name','false',NULL,NULL,NULL,NULL)
-,(3,'role','false','roleAction','false','false','false','false','true','false','false','false','roleAction','false',NULL,NULL,NULL,NULL)
-,(4,'role','false','roleType','false','false','false','false','true','false','false','false','Role Type','false',NULL,NULL,NULL,NULL)
-,(5,'role','false','description','false','false','false','false','true','false','false','false','Description','false',NULL,NULL,NULL,NULL)
-,(7,'role','false','isJobRole','false','false','false','false','true','false','false','false','IsJobRole','false',NULL,NULL,NULL,NULL)
-,(8,'role','false','country','false','false','false','false','true','false','false','false','Country','false',NULL,NULL,NULL,NULL)
-,(9,'role','false','alias','false','false','false','false','true','false','false','false','Alias','false',NULL,NULL,NULL,NULL)
-,(10,'role','false','criticality','false','false','false','false','true','false','false','false','Criticality','false',NULL,NULL,NULL,NULL)
-,(11,'role','false','validity','false','false','false','false','true','false','false','false','Validity','false',NULL,NULL,NULL,NULL)
-,(12,'role','false','longDescription','false','false','false','false','true','false','false','false','Long Description','false',NULL,NULL,NULL,NULL)
-,(13,'role','false','userLocation','false','false','false','false','true','false','false','false','UserLocation','false',NULL,NULL,NULL,NULL)
-,(14,'role','false','city','false','false','false','false','true','false','false','false','City','false',NULL,NULL,NULL,NULL)
-,(15,'role','false','displayRoleName','false','false','false','false','true','false','false','false','Display Role Name','false',NULL,NULL,NULL,NULL)
-,(16,'role','false','evaluationStatus','false','false','false','false','true','false','false','false','Evaluation Status','false',NULL,NULL,NULL,NULL)
+INSERT INTO aehsc.field_def (id,class_ext_id,do_not_copy,field_name,is_base_class_field,is_dimension,is_list,is_measure,is_primitive,is_required,is_translated,is_unique,"label",no_history,reference_key,tooltip,"type",xtype,uri) VALUES 
+(1,'request','false','roleData','false','false','true','false','false','false','false','false','Role Data','false',NULL,NULL,'role',NULL,NULL)
+,(2,'role','false','roleName','false','false','false','false','true','false','false','false','roleName/Role Name','false',NULL,NULL,NULL,NULL,'IdentityRoles')
+,(3,'role','false','roleAction','false','false','false','false','true','false','false','false','roleAction','false',NULL,NULL,NULL,NULL,NULL)
+,(4,'role','false','roleType','false','false','false','false','true','false','false','false','Role Type','false',NULL,NULL,NULL,NULL,NULL)
+,(5,'role','false','description','false','false','false','false','true','false','false','false','Description','false',NULL,NULL,NULL,NULL,NULL)
+,(7,'role','false','isJobRole','false','false','false','false','true','false','false','false','IsJobRole','false',NULL,NULL,NULL,NULL,NULL)
+,(8,'role','false','country','false','false','false','false','true','false','false','false','Country','false',NULL,NULL,NULL,NULL,NULL)
+,(9,'role','false','alias','false','false','false','false','true','false','false','false','Alias','false',NULL,NULL,NULL,NULL,NULL)
+,(10,'role','false','criticality','false','false','false','false','true','false','false','false','Criticality','false',NULL,NULL,NULL,NULL,NULL)
+,(11,'role','false','validity','false','false','false','false','true','false','false','false','Validity','false',NULL,NULL,NULL,NULL,NULL)
+,(12,'role','false','longDescription','false','false','false','false','true','false','false','false','Long Description','false',NULL,NULL,NULL,NULL,NULL)
+,(13,'role','false','userLocation','false','false','false','false','true','false','false','false','UserLocation','false',NULL,NULL,NULL,NULL,NULL)
+,(14,'role','false','city','false','false','false','false','true','false','false','false','City','false',NULL,NULL,NULL,NULL,NULL)
+,(15,'role','false','displayRoleName','false','false','false','false','true','false','false','false','Display Role Name','false',NULL,NULL,NULL,NULL,NULL)
+,(16,'role','false','evaluationStatus','false','false','false','false','true','false','false','false','Evaluation Status','false',NULL,NULL,NULL,NULL,NULL)
 ;
 
 INSERT INTO aehsc.lists (id,int_status,changed_by,changed_on,created_by,created_on,description,code) VALUES  
